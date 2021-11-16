a0ae4ce4f7b0e5afb822b911a33fd8b35141c2cc
yujuhun <yjh6487@hanmail.net>
2021-11-16 16:14:04 +0900

    4. Multiple Mapping Servlet

---
+++ pro05/src/sec01/ex01/SecondServlet.java
@@ -0,0 +1,22 @@
+package sec01.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecondServlet  extends HttpServlet{//inherit Httpservlet 
+	   public void init() throws ServletException { //Servlet creation
+	      System.out.println("init Call the method>>>>");
+	   }
+
+	   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  ServletException, IOException { //Request processed
+	      System.out.println("doGet Call the method>>>>");
+	   } 
+
+	   public void destroy() { //Servlet disappears
+	      System.out.println("destroy Call the method>>>>");
+	   }
+} // Multiple Mapping Servlet 
