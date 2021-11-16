d2b27a69a158a4da7cba44c92580d3e43d6900ab
yujuhun <yjh6487@hanmail.net>
2021-11-16 18:10:04 +0900

    6. Simple login form using Servlet

---
+++ pro06/src/sec01/ex01/LoginServlet.java
@@ -0,0 +1,45 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init Call the method");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy Call the method");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 request.setCharacterEncoding("utf-8"); // Encoding format
+	      String user_id=request.getParameter("user_id");
+	      String user_pw=request.getParameter("user_pw"); // Received the transmitted value
+	      System.out.println("Id:"+user_id);
+	      System.out.println("Password:"+user_pw);
+	}
+
+} // simple login form using Servlet
