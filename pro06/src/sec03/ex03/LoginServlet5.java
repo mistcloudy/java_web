c2a738942b6b7250c05845d13fd4666b6ffe4ded
yujuhun <yjh6487@hanmail.net>
2021-11-18 13:55:34 +0900

    13. Request to Servlet through JavaScript

---
+++ pro06/src/sec03/ex03/LoginServlet5.java
@@ -0,0 +1,61 @@
+package sec03.ex03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet5
+ */
+@WebServlet("/login5")
+public class LoginServlet5 extends HttpServlet {
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init Call the method");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy Call the method");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		String address = request.getParameter("user_address"); // Receive hidden tag
+		System.out.println("아이디   : " + id);
+		System.out.println("비밀번호 : " + pw);
+
+		String data = "<html>"; // Print Web browser
+		data += "<body>";
+		data += "Id : " + id;
+		data += "<br>";
+		data += "Password : " + pw;
+		data += "<br>";
+		data += "Address : " + address;
+		data += "</body>";
+		data += "</html>";
+		out.print(data);
+	}
+
+} // Request to Servlet through JavaScript
