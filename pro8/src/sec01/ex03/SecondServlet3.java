bc4df817bd046e91ee3e71fdbd192edbab028088
yujuhun <yjh6487@hanmail.net>
2021-11-19 17:12:13 +0900

    23.  Call due to location

---
+++ pro8/src/sec01/ex03/SecondServlet3.java
@@ -0,0 +1,30 @@
+package sec01.ex03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/second3")
+public class SecondServlet3 extends HttpServlet {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html;charset=utf-8");
+	      PrintWriter out = response.getWriter();
+
+	      out.println("<html><body>");
+	      out.println("This is a redirect practice using location.");
+	      out.println("</body></html>");
+	}
+} // Call due to location
+
