7da51eca4b72a8328061e3f0f4e24173c8ceccb4
yujuhun <yjh6487@hanmail.net>
2021-11-19 16:13:02 +0900

    21. Call another servlet with redirect

---
+++ pro8/src/sec01/ex01/SecondServlet.java
@@ -0,0 +1,29 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html;charset=utf-8");
+	      PrintWriter out = response.getWriter();
+
+	      out.println("<html><body>");
+	      out.println("This is a redirect practice using sendRedirect.");
+	      out.println("</body></html>");
+	}
+} // Call due to redirect
