896f51d531f92aed138912550e45e521bf843025
yujuhun <yjh6487@hanmail.net>
2021-11-19 16:19:53 +0900

    22. Call another servlet with refresh

---
+++ pro8/src/sec01/ex02/SecondServlet2.java
@@ -0,0 +1,29 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/second2")
+public class SecondServlet2 extends HttpServlet {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html;charset=utf-8");
+	      PrintWriter out = response.getWriter();
+
+	      out.println("<html><body>");
+	      out.println("This is a redirect practice using refresh.");
+	      out.println("</body></html>");
+	}
+} // Call due to refresh
